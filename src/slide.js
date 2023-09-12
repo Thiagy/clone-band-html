d3b83240dec03267ee473a8879837fa99406aa80
Thiago Rodrigues Teixeira <thiagotx2019@gmail.com>
2023-09-12 02:26:58 -0300

    ok

--- src/slide.js
+++ src/slide.js
@@ -1,100 +1,85 @@
-// Esta função consome uma API que retorna notícias com título/conteúdo/imagens
-async function getNews() {
-    try {
-        const response = await fetch('https://g1-clone-node-react.onrender.com/news');
-        if (!response.ok) {
-            throw new Error('Erro ao obter dados da API');
-        }
-        
-        const news = await response.json();
-
-        // Converte o objeto news em uma string JSON antes de armazená-lo em sessionStorage
-        sessionStorage.setItem('newsArray', JSON.stringify(news));
-
-        return news;
-    } catch (error) {
-        console.error('Ocorreu um erro:', error);
-        throw error; 
-    }
-}
-
-var index_news = 0;
-async function changesNews() {
-    const news = sessionStorage.getItem('arrayNew');
-
-    if (news) {
-        // Convert a string to an array (assuming your data is in a JSON format)
-        const newsArray = JSON.parse(news);
-
-        // Verifique se o índice não ultrapassa o comprimento da matriz
-        if (index_news + 14 < newsArray.length) {
-            const column_news_1 = newsArray.slice(index_news, index_news + 4);
-            const column_news_2 = newsArray.slice(index_news + 5, index_news + 9);
-            const column_news_3 = newsArray.slice(index_news + 10, index_news + 14);
-            const column_news_4 = newsArray.slice(index_news, index_news + 4);
-
-            // Selecione todos os elementos com a classe CSS '.flex-bar'
-            const flexBars = document.querySelectorAll('.flex-bar');
-
-            flexBars[1].innerText = ''
-            column_news_1.forEach(n => {
-                createHtmlNews(n, 1);
-            });
-
-            flexBars[2].innerText = ''
-
-            column_news_2.forEach(n => {
-                createHtmlNews(n, 2);
-            });
-
-            flexBars[3].innerText = ''
-            column_news_3.forEach(n => {
-                createHtmlNews(n, 3);
-            });
-
-            flexBars[5].innerText = ''
-            column_news_4.forEach(n => {
-                createHtmlNews(n, 5);
-            });
-
-            index_news += 15; // Você está pulando 15 elementos de cada vez
-        } else {
-            index_news = 0;
-        }
-    }
-}
+import { getNews } from "./newApi.js";
 
 //Aqui gera uma caixa de notícias com título/conteúdo/imagem
 function createHtmlNews(newOne, index){
 
-    const flex_box_new = document.createElement('div')
-    const div_img = document.createElement('div')
-    const div_title = document.createElement('div')
-    const div_content = document.createElement('div')
-  
-    const img = document.createElement('img')
-    const title = document.createElement('h5')
-    const content = document.createElement('h3')
+  const flex_box_new = document.createElement('div')
+  const div_img = document.createElement('div')
+  const div_title = document.createElement('div')
+  const div_content = document.createElement('div')
 
-    flex_box_new.setAttribute('class', 'flex-box-new')
-    flex_box_new.style.cursor='pointer'
+  const img = document.createElement('img')
+  const title = document.createElement('h5')
+  const content = document.createElement('h3')
 
-    img.src=newOne.image
-    img.alt=newOne.content
-    title.innerText=newOne.title
-    content.innerText=newOne.content
+  flex_box_new.setAttribute('class', 'flex-box-new')
+  flex_box_new.style.cursor='pointer'
 
-    div_img.append(img)
-    div_title.append(title)
-    div_content.append(content)
+  img.src=newOne.image
+  img.alt=newOne.content
+  title.innerText=newOne.title
+  content.innerText=newOne.content
 
-    flex_box_new.append(div_img, div_title, div_content)
-    document.getElementsByClassName('flex-bar')[index].appendChild(flex_box_new)
+  div_img.append(img)
+  div_title.append(title)
+  div_content.append(content)
+
+  flex_box_new.append(div_img, div_title, div_content)
+  document.getElementsByClassName('flex-bar')[index].appendChild(flex_box_new)
 }
 
-setInterval(async ()=>{
-    await changesNews();
-}, 5000)
+var index_news = 0;
+async function changesNews() {
+
+  const spinner = document.querySelector('#spinner')
+  const main = document.querySelector('main')
+  const footer = document.querySelector('footer')
+  const news = await getNews()
+
+    
+  if (news) {
+      spinner.style.display='none'
+      main.style.display='flex'
+      footer.style.display='flex'
+
+    // Verifique se o índice não ultrapassa o comprimento da matriz
+    if (index_news + 14 < news.length) {
+        const column_news_1 = news.slice(index_news, index_news + 4);
+        const column_news_2 = news.slice(index_news + 5, index_news + 9);
+        const column_news_3 = news.slice(index_news + 10, index_news + 14);
+        const column_news_4 = news.slice(index_news, index_news + 4);
+
+        // Selecione todos os elementos com a classe CSS '.flex-bar'
+        const flexBars = document.querySelectorAll('.flex-bar');
+
+        flexBars[1].innerText = ''
+        column_news_1.forEach(n => {
+            createHtmlNews(n, 1);
+        });
+
+        flexBars[2].innerText = ''
+
+        column_news_2.forEach(n => {
+            createHtmlNews(n, 2);
+        });
+
+        flexBars[3].innerText = ''
+        column_news_3.forEach(n => {
+            createHtmlNews(n, 3);
+        });
+
+        flexBars[5].innerText = ''
+        column_news_4.forEach(n => {
+            createHtmlNews(n, 5);
+        });
+
+        index_news += 15; // Você está pulando 15 elementos de cada vez
+    } else {
+        index_news = 0;
+    }
+  }
+}
+
 
 //Array de horóscopos
 const horoscopos = [
@@ -173,17 +158,25 @@ container.innerHTML = criarHTMLHoroscopos(horoscopos);
 
 var count = 1;
 document.getElementById("radio1").checked = true;
-let navigation_btn = document.getElementsByClassName("navigation-btn")
 
 function nextImage(){
     count++;
-    if(count>4){
+
+    if(count > 4){
         count = 1;
     }
+
     document.getElementById("radio"+count).checked=true;
-    
 }
 
-setInterval( function(){
+
+setInterval( async()=>{
     nextImage();
+    await changesNews();
+   
+
+  
 }, 5000)
+
+
+
